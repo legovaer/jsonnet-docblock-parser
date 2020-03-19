43988c4a7864cd82b3fb952c83f3f5101a375a89
Levi Govaerts <levi@scalecity.io>
2020-03-19 16:38:48 +0100

    Added minor fixes

---
+++ README.md
@@ -0,0 +1,10 @@
+# Jsonnet Docblock Parser
+
+[![Build Status](https://travis-ci.com/legovaer/jsonnet-docblock-parser.svg?branch=master)](https://travis-ci.com/legovaer/jsonnet-docblock-parser)
+[![PyPI status](https://img.shields.io/pypi/status/jsonnet_docblock_parser.svg)](https://pypi.python.org/pypi/jsonnet_docblock_parser/)
+[![PyPI version shields.io](https://img.shields.io/pypi/v/jsonnet_docblock_parser.svg)](https://pypi.python.org/pypi/jsonnet_docblock_parser/)
+[![PyPI license](https://img.shields.io/pypi/l/jsonnet_docblock_parser.svg)](https://pypi.python.org/pypi/jsonnet_docblock_parser/)
+[![PyPI pyversions](https://img.shields.io/pypi/pyversions/jsonnet_docblock_parser.svg)](https://pypi.python.org/pypi/jsonnet_docblock_parser/)
+
+Simple, powerfull jsonnet docblock parser that returns a JSON representation to work with.
+
--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
-import setuptools
-
-setuptools.setup()
+from distutils.core import setup
+setup(
+    name='jsonnet_docblock_parser',
+    packages=['jsonnet_docblock_parser'],
+    version='0.1.0',
+    license='MIT',
+    description='Simple, powerfull jsonnet docblock parser that returns a JSON representation to work with.',
+    author='Levi Govaerts',
+    author_email='legovaer@me.com',
+    url='https://github.com/legovaer/jsonnet-docblock-parser',
+    download_url='https://github.com/legovaer/jsonnet-docblock-parser',
+    keywords=['jsonnet', 'docblock', 'json', 'parser'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
